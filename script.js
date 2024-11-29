c7f35ba00cb7e20087bc5c144d5172e130dbb68b
antarakanswal1 <antarakanswal371@gmail.com>
2024-11-29 18:03:25 +0530

    filter

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ themeToggle.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
 });
 
+function filterItems(category) {
+  const items = document.querySelectorAll('.product-card');
+
+  items.forEach(item => {
+      if (category === 'all') {
+          item.style.display = 'block';
+      } else if (item.classList.contains(category)) {
+          item.style.display = 'block';
+      } else {
+          item.style.display = 'none';
+      }
+  });
+}
